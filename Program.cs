727c8223130dea3a7b36f42eb0b73cc8a035cabc
SURFT!$ <152164319+Surft14@users.noreply.github.com>
2024-09-05 20:02:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcv2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            char op;
+            Console.Write("enter number 1 = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("enter number 2 = ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("enter operator = ");
+            op = char.Parse(Console.ReadLine());
+
+            if (op == '+')
+            {
+                Console.WriteLine("Answer = " + (a + b));
+            }
+            else if(op == '-')
+            {
+                Console.WriteLine("Answer = " + (a - b));
+            }
+            else if(op == '*')
+            {
+                Console.WriteLine("Answer = " + (a * b));
+            }
+            else if(op == '/')
+            {
+                Console.WriteLine("Answer = " + (a / b));
+            }
+
+        }
+    }
+}
